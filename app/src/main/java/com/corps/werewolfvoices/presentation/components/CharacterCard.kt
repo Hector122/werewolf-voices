b97ca842e98091a449b123baad0ca10b944eb375
Hector Castillo <hector122@gmail.com>
2026-03-10 17:10:34 -0400

    Improve character model

---
+++ app/src/main/java/com/corps/werewolfvoices/presentation/components/CharacterCard.kt
@@ -0,0 +1,8 @@
+package com.corps.werewolfvoices.presentation.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CharacterCard(){
+
+}
---
+++ app/src/main/java/com/corps/werewolfvoices/presentation/models/CharacterUi.kt
@@ -0,0 +1,14 @@
+package com.corps.werewolfvoices.presentation.models
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.RawRes
+import com.corps.werewolfvoices.domain.model.CharacterType
+
+data class CharacterUi(
+    val id: Int,
+    val name: String,
+    val type: CharacterType,
+    val description: String,
+    @DrawableRes val imageRes: Int,
+    @RawRes val soundRes: Int
+)
